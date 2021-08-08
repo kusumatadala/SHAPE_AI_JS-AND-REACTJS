b23b9808fca543c9966ad1420a8b23803c6f2450
kusumatadala <65664333+kusumatadala@users.noreply.github.com>
2021-08-08 17:59:50 +0530

    Add files via upload

---
+++ Boot.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+function Boot() {
+  return (
+    <div className="note">
+      <h1>ReactJs</h1>
+      <p>
+        Topics : JS and ReactJs
+        <br />
+        Duration : Seven days
+        <br />
+        Instructor : Shaurya Sinha
+        <br />
+        Result : This website
+      </p>
+    </div>
+  );
+}
+
+export default Boot;
